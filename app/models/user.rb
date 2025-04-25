0572ab423da0215136beac324cf1369bdfbc7d9e
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-04-25 08:12:54 +0000

    corrige les erruers

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,12 +33,15 @@ class User < ApplicationRecord
   has_many :own_goals, foreign_key: "owner_id", class_name: "Goal"
   has_many :sent_friend_requests, foreign_key: :sender_id, class_name: "FriendRequest"
   has_many :accepted_sent_friend_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FriendRequest"
+  has_many :pending_sent_friend_requests, -> { pending }, foreign_key: :sender_id, class_name: "FriendRequest"
 
   has_many :received_friend_requests, foreign_key: :recipient_id, class_name: "FriendRequest"
   has_many :accepted_received_friend_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FriendRequest"
+  has_many :pending_received_friend_requests, -> { pending }, foreign_key: :recipient_id, class_name: "FriendRequest"
 
   has_many :leaders, through: :accepted_sent_friend_requests, source: :recipient
   has_many :followers, through: :accepted_received_friend_requests, source: :sender
+  has_many :pending, through: :pending_received_friend_requests, source: :sender
 
   has_many :feed, through: :leaders, source: :own_goals
 
