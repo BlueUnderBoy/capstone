5da32cf87dd6134c3a223837eaacbdd79e106227
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-04-27 23:54:23 +0000

    met a jour

--- app/controllers/finds_controller.rb
+++ app/controllers/finds_controller.rb
@@ -1,17 +1,19 @@
 class FindsController < ApplicationController
 
   def search
-    if params[:find].present?
-      @uname = params[:find]
-      @results = User.where('LOWER(username) LIKE ?', "%#{@uname.downcase}%")
-      if @results.any?
-        @user = User.find_by!(username: @uname)
-        render "users/show"
-      else 
-        format.html { redirect_back fallback_location: root_url, notice: "User does not exist." }
+    respond_to do |format|
+      if params[:find].present?
+        @uname = params[:find]
+        @results = User.where('LOWER(username) LIKE ?', "%#{@uname.downcase}%")
+        if @results.any?
+          @user = User.find_by!(username: @uname)
+          format.html { render "users/show" }
+        else 
+          format.html { redirect_back fallback_location: root_url, alert: "User does not exist." }
+        end
+      else
+        format.html { redirect_back fallback_location: root_url, alert: "User does not exist." }
       end
-    else
-      format.html { redirect_back fallback_location: root_url, notice: "User does not exist." }
     end
 
   end
