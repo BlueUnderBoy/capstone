ac6b8f9baed7b78135a0bd908fa0b12dc6e9fcda
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-05-07 23:00:47 +0000

    met a jour la page de landing

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get ":username/pending" => "users#pending", as: :pending
   
-  get ":username/feed" => "users#feed"
+  get ":username/feed" => "users#feed", as: :feed
   
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
